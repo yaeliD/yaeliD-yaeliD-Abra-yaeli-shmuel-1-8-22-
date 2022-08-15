73281dc3dc049140dfe2e0408b1f014c88199181
yaeliD <yaelid1999@gmail.com>
2022-08-15 18:05:38 +0300

    change key

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class WeatherService {
 
   URL = 'http://dataservice.accuweather.com';
-  API_KEY = 'SbelAxqVDWiQVCN0AnbU8mJWCWQZt539';
+  API_KEY = 'mZubytyHL1AkRgmuAPiDF5hFDjy0Il3Y';
 
   constructor(private http: HttpClient) { }
 
